bc72091968ca002d7988042e9b0166b9e502ab6a
Décio Francis <francisdasilva@gmail.com>
2024-04-03 23:20:13 -0300

    feat: develop function to create user and handle error

--- theme/api/endpoints/user_post.php
+++ theme/api/endpoints/user_post.php
@@ -2,10 +2,26 @@
 
 function api_user_post($request) {
 
-    $response = [
-        'ID' => '2',
-        'user_login' => 'meu_usuario',
-    ];
+    $email = sanitize_email($request['email']);
+    $username = sanitize_text_field($request['username']);
+    $password = $resquest['password'];
+
+    if(empty($email) || empty($username) || empty($password)) {
+        $response = new WP_Error('erro', 'Dados incompletos', ['status' => 406]);
+        return rest_ensure_response($response);
+    }
+
+    if(username_exists($username) || email_exists($email)) {
+        $response = new WP_Error('erro', 'Email ja cadastrado', ['status' => 406]);
+        return rest_ensure_response($response);
+    }
+
+    $response = wp_insert_user([
+        'user_login' => $username,
+        'user_email' => $email,
+        'user_pass' => $password,
+        'role' => 'subscriber',
+    ]);
 
     return rest_ensure_response($response);
 }
