6b2d447d7bd144221bfbe1a36f59fe92bd90689a
Décio Francis <francisdasilva@gmail.com>
2024-04-05 23:42:08 -0300

    feat: developer get function

--- theme/api/functions.php
+++ theme/api/functions.php
@@ -4,6 +4,7 @@ remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
