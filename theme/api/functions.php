208e07da014c61cb3202ea887db02ffc104a1295
Décio Francis <francisdasilva@gmail.com>
2024-04-11 21:56:32 -0300

    feat: create endpoint comment

--- theme/api/functions.php
+++ theme/api/functions.php
@@ -9,13 +9,15 @@ require_once $dirbase . '/endpoints/user_get.php';
 require_once $dirbase . '/endpoints/photo_post.php';
 require_once $dirbase . '/endpoints/photo_delete.php';
 
+require_once $dirbase . '/endpoints/comment_post.php';
+
 update_option('large_size_w', 1000);
 update_option('large_size_h', 1000);
 update_option('large_crop', 1);
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
-function change_api($slug) {
+function change_api() {
   return 'json';
 }
 add_filter('rest_url_prefix', 'change_api');
