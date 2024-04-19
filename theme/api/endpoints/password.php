f5e5c8cf5733cdf7fabe433f30889435b3f0197c
Décio Francis <francisdasilva@gmail.com>
2024-04-18 23:25:52 -0300

    feat: password reset changing password

--- theme/api/endpoints/password.php
+++ theme/api/endpoints/password.php
@@ -16,7 +16,7 @@ function api_password_lost($request) {
     }
 
     if(empty($user)) {
-        $response = new WP_Error('error', 'Usuario nao existe', ['status' => 401]);
+        $response = new WP_Error('error', 'Usuario nao existe.', ['status' => 401]);
         return rest_ensure_response($response);
     }
 
@@ -42,5 +42,40 @@ function register_api_password_lost() {
 }
 
 add_action('rest_api_init', 'register_api_password_lost');
+
+
+// Password Reset
+
+function api_password_reset($request) {
+    $login = $request['login'];
+    $password = $request['password'];
+    $key = $request['key'];
+    $user = get_user_by('login', $login);
+
+    if(empty($user)) {
+        $response = new WP_Error('error', 'Usuario nao existe.', ['status' => 401]);
+        return rest_ensure_response($response);
+    }
+
+    $check_key = check_password_reset_key($key, $login);
+
+    if(is_wp_erro($check_key)) {
+        $response = new WP_Error('error', 'Token expirado.', ['status' => 401]);
+        return rest_ensure_response($response);
+    }
+
+    reset_password($user, $password);
+
+    return rest_ensure_response('Senha alterada.');
+}
+
+function register_api_password_reset() {
+    register_rest_route('api', '/password/reset', [
+        'methods' => WP_REST_Server::CREATABLE,
+        'callback' => 'api_password_reset',
+    ]);
+}
+
+add_action('rest_api_init', 'register_api_password_reset');
 
 ?>
